b6ac2de32eaffcc823e0b08f546d8d53de08a420
Jason Lutz <jason.m.lutz@gmail.com>
2022-07-11 11:45:58 -0700

    tailwindcss install and config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.jsx",
+  ],
+}
